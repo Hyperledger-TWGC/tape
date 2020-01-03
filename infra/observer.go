3c0aba43fc184f3c03c88e3592c45ca8bf69f0d6
Jay Guo <guojiannan1101@gmail.com>
2020-01-03 17:26:39 +0800

    Fixed width print from observer
    Signed-off-by: Jay Guo <guojiannan1101@gmail.com>

--- infra/observer.go
+++ infra/observer.go
@@ -48,7 +48,7 @@ func (o *Observer) Start(N int, now time.Time) {
 
 		fb := r.Type.(*peer.DeliverResponse_FilteredBlock)
 		n = n + len(fb.FilteredBlock.FilteredTransactions)
-		fmt.Printf("Time %v\tBlock %d\tTx %d\n", time.Since(now), fb.FilteredBlock.Number, len(fb.FilteredBlock.FilteredTransactions))
+		fmt.Printf("Time %8.2fs\tBlock %6d\tTx %6d\n", time.Since(now).Seconds(), fb.FilteredBlock.Number, len(fb.FilteredBlock.FilteredTransactions))
 	}
 }
 
