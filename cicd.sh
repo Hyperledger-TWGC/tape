f2fa52e260d791649fac7073edbf31a8503d668f
Jay Guo <guojiannan1101@gmail.com>
2020-06-17 17:21:23 +0800

    Test build
    Signed-off-by: Jay Guo <guojiannan1101@gmail.com>

--- cicd.sh
+++ cicd.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
+set -e
 #git submodule update --init --recursive
-git clone https://github.com/hyperledger/fabric-samples.git
-cd fabric-samples
-curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh -o ./scripts/bootstrap.sh
-chmod +x ./scripts/bootstrap.sh
-./scripts/bootstrap.sh
-cd ./first-network
-sed -i 's/^askProceed/#askProceed/g' byfn.sh
-./byfn.sh up
-cd ../..
-cp -r ./fabric-samples/first-network/crypto-config .
+#git clone https://github.com/hyperledger/fabric-samples.git
+#cd fabric-samples
+DIR=$PWD
+curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash
+cd ./fabric-samples/first-network
+echo y | ./byfn.sh up
+cp -r crypto-config "$DIR" && cd "$DIR"
 go build
 ./stupid config.json 10
