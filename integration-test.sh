62bb2523ef9ace1888f4583025adc4607418d945
SamYuan1990 <yy19902439@126.com>
2020-06-29 19:48:06 +0800

    switch to test-network
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- integration-test.sh
+++ integration-test.sh
@@ -4,8 +4,11 @@ set -e
 DIR=$PWD
 
 curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash
-cd ./fabric-samples/first-network
-echo y | ./byfn.sh up
-cp -r crypto-config "$DIR" && cd "$DIR"
+cd ./fabric-samples/test-network
+echo y |  ./network.sh up createChannel
+echo y |  ./network.sh deployCC
+cp -r organizations "$DIR" && cd "$DIR"
 go build
 STUPID_LOGLEVEL=debug ./stupid config.json 100
+
+
