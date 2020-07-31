6fadaa09d73116faacb09f295cc29b1c5b578a07
SamYuan1990 <yy19902439@126.com>
2020-07-31 20:17:43 +0800

    verify for no TLS
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- pkg/infra/config_test.go
+++ pkg/infra/config_test.go
@@ -68,5 +68,63 @@ client_per_conn: 40`
 				ClientPerConn: 40,
 			}))
 		})
+
+		It("Load node without TLS", func() {
+			var configText = `
+org1peer0: &org1peer0
+  addr: peer0.org1.example.com:7051
+org2peer0: &org2peer0
+  addr: peer0.org2.example.com:7051
+org0orderer0: &org0orderer0
+  addr: orderer.example.com:7050
+
+endorsers:
+  - *org1peer0
+  - *org2peer0
+committer: *org2peer0
+orderer: *org0orderer0
+
+channel: mychannel
+chaincode: mycc
+args:
+  - invoke
+  - a
+  - b
+  - 1
+mspid: Org1MSP
+private_key: /path/to/private.key
+sign_cert: /path/to/sign.cert
+num_of_conn: 20
+client_per_conn: 40`
+
+			f, _ := ioutil.TempFile("", "config-*.yaml")
+			defer os.Remove(f.Name())
+			f.WriteString(configText)
+			f.Close()
+
+			c := infra.LoadConfig(f.Name())
+
+			Expect(c).To(Equal(infra.Config{
+				Endorsers: []infra.Node{
+					{Addr: "peer0.org1.example.com:7051"},
+					{Addr: "peer0.org2.example.com:7051"},
+				},
+				Committer:     infra.Node{Addr: "peer0.org2.example.com:7051"},
+				Orderer:       infra.Node{Addr: "orderer.example.com:7050"},
+				Channel:       "mychannel",
+				Chaincode:     "mycc",
+				Version:       "",
+				Args:          []string{"invoke", "a", "b", "1"},
+				MSPID:         "Org1MSP",
+				PrivateKey:    "/path/to/private.key",
+				SignCert:      "/path/to/sign.cert",
+				NumOfConn:     20,
+				ClientPerConn: 40,
+			}))
+
+			Expect(c.Endorsers[0].Addr).To(Equal("peer0.org1.example.com:7051"))
+			Expect(c.Endorsers[0].TLSCACert).To(Equal(""))
+
+		})
 	})
 })
