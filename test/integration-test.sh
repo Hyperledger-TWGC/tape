b91768f0ede75f14aae730bfd2a98337d12f6af2
Jay Guo <guojiannan1101@gmail.com>
2020-11-25 14:12:43 +0800

    Fix CI to pull Fabric release tar with correct version
    Signed-off-by: Jay Guo <guojiannan1101@gmail.com>

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -4,13 +4,13 @@ set -ex
 DIR=$PWD
 docker build -t tape:latest .
 
-curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.2.0 1.4.7
-
-cd ./fabric-samples/
 case $1 in
- 14)
+ 1_4)
+    # sadly, bootstrap.sh from release-1.4 still pulls binaries from Nexus, which is not available anymore
+    curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/release-2.2/scripts/bootstrap.sh | bash
+    cd ./fabric-samples/
     git checkout release-1.4
-    cd first-network
+    cd ./first-network
     echo y | ./byfn.sh up -i 1.4.8
     # comments here for 1.4.8 work around as docker image issue.
     # docker pull hyperledger/fabric-orderer:amd64-1.4  
@@ -19,8 +19,9 @@ case $1 in
     
     CONFIG_FILE=/config/test/config14org1andorg2.yaml
     ;;
- 22)
-    cd ./test-network
+ 2_2)
+    curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/release-2.2/scripts/bootstrap.sh | bash
+    cd ./fabric-samples/test-network
     echo y |  ./network.sh down -i 2.2
     echo y |  ./network.sh up createChannel -i 2.2
     cp -r organizations "$DIR"
@@ -34,10 +35,26 @@ case $1 in
 
     echo y |  ./network.sh deployCC "${ARGS[@]}"
     ;;
+ latest)
+    curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash
+    cd ./fabric-samples/test-network
+    echo y |  ./network.sh down
+    echo y |  ./network.sh up createChannel
+    cp -r organizations "$DIR"
+
+    CONFIG_FILE=/config/test/config20org1andorg2.yaml
+
+    if [ $2 == "ORLogic" ]; then
+      CONFIG_FILE=/config/test/config20selectendorser.yaml
+      ARGS=(-ccep "OR('Org1.member','Org2.member')")
+    fi
+
+    echo y |  ./network.sh deployCC "${ARGS[@]}"
+    ;;
  *)
-    echo "Usage: $1 [14|22]"
+    echo "Usage: $1 [1_4|2_2|latest]"
     echo "When given version, start byfn or test network basing on specific version of docker image"
-    echo "For any value without mock, 14, 22 will show this hint"
+    echo "For any value without mock, 1_4, 2_2, latest will show this hint"
     exit 0
     ;;
 esac
