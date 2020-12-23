28e5294a56d72e5e88243426b8b33dd6c027c71b
wuxu <wuxu1103@163.com>
2020-12-23 20:23:14 +0800

    add Makefile
    add commit SHA and built time in "tape version"
    
    Signed-off-by: wuxu <wuxu1103@163.com>

--- pkg/infra/version.go
+++ pkg/infra/version.go
@@ -7,17 +7,24 @@ import (
 
 const (
 	programName = "tape"
-	version     = "0.0.2"
+)
+
+var (
+	Version   string = "latest"
+	CommitSHA string = "development build"
+	BuiltTime string = "Mon Dec 21 19:00:00 2020"
 )
 
 // GetVersionInfo return version information
 // TODO add commit hash, Built info
 func GetVersionInfo() string {
 	return fmt.Sprintf(
-		"%s:\n Version: %s\n Go version: %s\n OS/Arch: %s\n",
+		"%s:\n Version: %s\n Go version: %s\n Git commit: %s\n Built: %s\n OS/Arch: %s\n",
 		programName,
-		version,
+		Version,
 		runtime.Version(),
+		CommitSHA,
+		BuiltTime,
 		fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 	)
 }
