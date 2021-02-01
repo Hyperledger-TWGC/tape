d3933be2b2e1de48e3f643ce6c030dc9468f2db9
SamYuan1990 <yy19902439@126.com>
2021-02-01 18:44:26 +0800

    reslove for #144 and upgrade 1.4 to 1.4.10
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -11,13 +11,30 @@ case $1 in
     cd ./fabric-samples/
     git checkout release-1.4
     cd ./first-network
-    echo y | ./byfn.sh up -i 1.4.8
+    # 1.4.10
+    echo y | ./byfn.sh up -i 1.4.10
     # comments here for 1.4.8 work around as docker image issue.
     # docker pull hyperledger/fabric-orderer:amd64-1.4  
     # Error response from daemon: manifest for hyperledger/fabric-orderer:amd64-1.4 not found: manifest unknown: manifest unknown
     cp -r crypto-config "$DIR"
     
     CONFIG_FILE=/config/test/config14org1andorg2.yaml
+    ;;
+ 2_2)
+    curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.2 1.4.9
+    cd ./fabric-samples/test-network
+    echo y |  ./network.sh down -i 2.2
+    echo y |  ./network.sh up createChannel -i 2.2
+    cp -r organizations "$DIR"
+
+    CONFIG_FILE=/config/test/config20org1andorg2.yaml
+
+    if [ $2 == "ORLogic" ]; then
+      CONFIG_FILE=/config/test/config20selectendorser.yaml
+      ARGS=(-ccep "OR('Org1.member','Org2.member')")
+    fi
+
+    echo y |  ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go/ -ccl go "${ARGS[@]}"
     ;;
  2_3)
     curl -vsS https://raw.githubusercontent.com/hyperledger/fabric/release-2.3/scripts/bootstrap.sh | bash
@@ -52,9 +69,9 @@ case $1 in
     echo y |  ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go/ -ccl go "${ARGS[@]}"
     ;;
  *)
-    echo "Usage: $1 [1_4|2_3|latest]"
+    echo "Usage: $1 [1_4|2_2|2_3|latest]"
     echo "When given version, start byfn or test network basing on specific version of docker image"
-    echo "For any value without mock, 1_4, 2_3, latest will show this hint"
+    echo "For any value without mock, 1_4, 2_2, 2_3, latest will show this hint"
     exit 0
     ;;
 esac
