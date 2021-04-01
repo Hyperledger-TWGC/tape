828ffec1d30c6cf6b0f1f64cd47ca1b4d776775a
yiwenlong <wlong.yi@gmail.com>
2021-04-01 17:54:51 +0800

    Modify variable naming spelling errors.
    Signed-off-by: yiwenlong <wlong.yi@gmail.com>

--- pkg/infra/process.go
+++ pkg/infra/process.go
@@ -24,7 +24,7 @@ func Process(configPath string, num int, burst int, rate float64, logger *log.Lo
 	done := make(chan struct{})
 	finishCh := make(chan struct{})
 	errorCh := make(chan error, burst)
-	assember := &Assembler{Signer: crypto}
+	assembler := &Assembler{Signer: crypto}
 	blockCollector, err := NewBlockCollector(config.CommitThreshold, len(config.Committers))
 	if err != nil {
 		return errors.Wrap(err, "failed to create block collector")
@@ -34,15 +34,15 @@ func Process(configPath string, num int, burst int, rate float64, logger *log.Lo
 	}
 
 	for i := 0; i < 5; i++ {
-		go assember.StartSigner(raw, signed, errorCh, done)
-		go assember.StartIntegrator(processed, envs, errorCh, done)
+		go assembler.StartSigner(raw, signed, errorCh, done)
+		go assembler.StartIntegrator(processed, envs, errorCh, done)
 	}
 
-	proposors, err := CreateProposers(config.NumOfConn, config.ClientPerConn, config.Endorsers, logger)
+	proposers, err := CreateProposers(config.NumOfConn, config.ClientPerConn, config.Endorsers, logger)
 	if err != nil {
 		return err
 	}
-	proposors.Start(signed, processed, done, config)
+	proposers.Start(signed, processed, done, config)
 
 	broadcaster, err := CreateBroadcasters(config.NumOfConn, config.Orderer, logger)
 	if err != nil {
