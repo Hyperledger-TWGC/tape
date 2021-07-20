fc59ebbfdc9c4df1f71592eeb0915574fb140e17
Sam Yuan <yy19902439@126.com>
2021-07-21 00:56:04 +0800

    Arch refactor alpha (#190)
    * add crpto interface as repare for #127
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * adding Worker interface for #56 and decouple Assembler and Integrator
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * refactor for worker interface
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * package structure
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * fix up
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * package refactor
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * fix up
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * fix up
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * remove Envelope from elements
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * add memeory free
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * remove Proposal from Elements
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * fix up
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * move start time to ctx
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- pkg/infra/observer/observerFactory.go
+++ pkg/infra/observer/observerFactory.go
@@ -9,17 +9,17 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func CreateObserverWorkers(config basic.Config, crypto infra.Crypto, blockCh chan *AddressedBlock, logger *log.Logger, ctx context.Context, finishCh chan struct{}, num int, errorCh chan error) ([]infra.Worker, *Observers, error) {
+func CreateObserverWorkers(config basic.Config, crypto infra.Crypto, blockCh chan *AddressedBlock, logger *log.Logger, ctx context.Context, finishCh chan struct{}, num int, errorCh chan error) ([]infra.Worker, error) {
 	observer_workers := make([]infra.Worker, 0)
 	blockCollector, err := NewBlockCollector(config.CommitThreshold, len(config.Committers), ctx, blockCh, finishCh, num, true)
 	if err != nil {
-		return observer_workers, nil, errors.Wrap(err, "failed to create block collector")
+		return observer_workers, errors.Wrap(err, "failed to create block collector")
 	}
 	observer_workers = append(observer_workers, blockCollector)
 	observers, err := CreateObservers(ctx, crypto, errorCh, blockCh, config, logger)
 	if err != nil {
-		return observer_workers, observers, err
+		return observer_workers, err
 	}
 	observer_workers = append(observer_workers, observers)
-	return observer_workers, observers, nil
+	return observer_workers, nil
 }
