aec9664314d01785b29e74d61645f6f8024111af
Sam Yuan <yy19902439@126.com>
2021-08-06 20:37:15 +0800

    adding for commit phase only dummy impl (#195)
    * adding for commit phase only dummy impl
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * fix up
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * impl for endorsement only
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * fix up
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * fix up
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>

---
+++ pkg/infra/trafficGenerator/fackEnvelopGenerator.go
@@ -0,0 +1,58 @@
+package trafficGenerator
+
+import (
+	"tape/internal/fabric/protoutil"
+	"tape/pkg/infra"
+	"tape/pkg/infra/basic"
+
+	"github.com/hyperledger/fabric-protos-go/common"
+)
+
+type fackEnvelopGenerator struct {
+	Num     int
+	Burst   int
+	R       float64
+	Config  basic.Config
+	Crypto  infra.Crypto
+	Envs    chan *common.Envelope
+	ErrorCh chan error
+}
+
+func (initiator *fackEnvelopGenerator) Start() {
+	for i := 0; i < initiator.Num; i++ {
+		nonce := []byte("nonce-abc-12345")
+		creator, _ := initiator.Crypto.Serialize()
+		txid := protoutil.ComputeTxID(nonce, creator)
+
+		txType := common.HeaderType_ENDORSER_TRANSACTION
+		chdr := &common.ChannelHeader{
+			Type:      int32(txType),
+			ChannelId: initiator.Config.Channel,
+			TxId:      txid,
+			Epoch:     uint64(0),
+		}
+
+		shdr := &common.SignatureHeader{
+			Creator: creator,
+			Nonce:   nonce,
+		}
+
+		payload := &common.Payload{
+			Header: &common.Header{
+				ChannelHeader:   protoutil.MarshalOrPanic(chdr),
+				SignatureHeader: protoutil.MarshalOrPanic(shdr),
+			},
+			Data: []byte("data"),
+		}
+		payloadBytes, _ := protoutil.GetBytesPayload(payload)
+
+		signature, _ := initiator.Crypto.Sign(payloadBytes)
+
+		envelope := &common.Envelope{
+			Payload:   payloadBytes,
+			Signature: signature,
+		}
+
+		initiator.Envs <- envelope
+	}
+}
