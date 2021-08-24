dc01e3a7c5cb29d6816910b5478be3698d3a6335
Sam Yuan <yy19902439@126.com>
2021-08-24 23:13:23 +0800

    impl for #200
    Signed-off-by: Sam Yuan <yy19902439@126.com>

---
+++ pkg/infra/trafficGenerator/proposal_test.go
@@ -0,0 +1,64 @@
+package trafficGenerator_test
+
+import (
+	"regexp"
+	"strconv"
+	"tape/pkg/infra/trafficGenerator"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Proposal", func() {
+	Context("ConvertString", func() {
+		It("work accordingly for string", func() {
+			input := "data"
+			data, err := trafficGenerator.ConvertString(input)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(data).To(Equal("data"))
+		})
+
+		It("work accordingly for random str", func() {
+			input := "randomString1"
+			data, err := trafficGenerator.ConvertString(input)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(data)).To(Equal(1))
+		})
+
+		It("work accordingly for uuid", func() {
+			input := "uuid"
+			data, err := trafficGenerator.ConvertString(input)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(data) > 0).To(BeTrue())
+		})
+
+		It("work accordingly for randomNumber", func() {
+			input := "randomNumber1_9"
+			data, err := trafficGenerator.ConvertString(input)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(data)).To(Equal(1))
+			num, err := strconv.Atoi(data)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(num > 0).To(BeTrue())
+		})
+
+		It("work accordingly for string mix mode", func() {
+			input := "{\"key\":\"randomNumber1_50\",\"key1\":\"randomNumber1_20\"}"
+			data, err := trafficGenerator.ConvertString(input)
+			Expect(err).NotTo(HaveOccurred())
+			regString, err := regexp.Compile("{\"key\":\"\\d*\",\"key1\":\"\\d*\"}")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(regString.MatchString(data)).To(BeTrue())
+		})
+
+		It("work accordingly for string mix mode 2", func() {
+			input := "{\"k1\":\"uuid\",\"key2\":\"randomNumber10000_20000\",\"keys\":\"randomString10\"}"
+			data, err := trafficGenerator.ConvertString(input)
+			Expect(err).NotTo(HaveOccurred())
+			regString, err := regexp.Compile("{\"k1\":\".*\",\"key2\":\"\\d*\",\"keys\":\".*\"}")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(regString.MatchString(data)).To(BeTrue())
+		})
+
+	})
+})
