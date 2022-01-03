80b9c169393fa2331e0b732aa4a0425eda597dbd
Sam Yuan <yy19902439@126.com>
2022-01-03 14:31:15 +0800

    fix up
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- pkg/infra/trafficGenerator/policyHandler.go
+++ pkg/infra/trafficGenerator/policyHandler.go
@@ -6,24 +6,10 @@ import (
 	"github.com/open-policy-agent/opa/rego"
 )
 
-func Check(input []string) (bool, error) {
-	// Create query that returns a single boolean value.
+func Check(input []string, rule string) (bool, error) {
 	rego := rego.New(
 		rego.Query("data.tape.allow"),
-		rego.Module("",
-			`package tape
-
-default allow = false
-allow {
-	input[_] == "org1"
-}
-
-allow {
-	input[_] == "org2"
-}
-
-`,
-		),
+		rego.Module("", rule),
 		rego.Input(input),
 	)
 	rs, err := rego.Eval(context.Background())
--- pkg/infra/trafficGenerator/policyHandler_test.go
+++ pkg/infra/trafficGenerator/policyHandler_test.go
@@ -7,19 +7,72 @@ import (
 )
 
 var _ = Describe("PolicyHandler", func() {
-	It("should pass", func() {
+	It("should pass when org1 with rule org1 or org2", func() {
 		input := make([]string, 2)
 		input[0] = "org1"
 		//input[1] = "org2"
-		rs, err := trafficGenerator.Check(input)
+		rule := `package tape
+
+		default allow = false
+		allow {
+			input[_] == "org1"
+		}
+		
+		allow {
+			input[_] == "org2"
+		}`
+
+		rs, err := trafficGenerator.Check(input, rule)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(rs).To(BeTrue())
 	})
 
-	It("should not pass", func() {
+	It("should not pass when null with rule org1 or org2", func() {
 		input := make([]string, 2)
-		rs, err := trafficGenerator.Check(input)
+		rule := `package tape
+
+		default allow = false
+		allow {
+			input[_] == "org1"
+		}
+		
+		allow {
+			input[_] == "org2"
+		}`
+		rs, err := trafficGenerator.Check(input, rule)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(rs).To(BeFalse())
+	})
+
+	It("should not pass when org1 with rule org1 and org2", func() {
+		input := make([]string, 2)
+		input[0] = "org1"
+		rule := `package tape
+
+		default allow = false
+		allow {
+			input[_] == "org1"
+			input[_] == "org2"
+		}`
+		rs, err := trafficGenerator.Check(input, rule)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(rs).To(BeFalse())
+	})
+
+	It("should pass with rule org1 and org2", func() {
+		input := make([]string, 2)
+		input[0] = "org1"
+		input[1] = "org2"
+		rule := `package tape
+
+		default allow = false
+		allow {
+			input[_] == "org1"
+			input[_] == "org2"
+		}
+		`
+		rs, err := trafficGenerator.Check(input, rule)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(rs).To(BeTrue())
 	})
 })
